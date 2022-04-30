9cff4263e19783ed8ef0dc36d8c76c1e4f1691ab
Sanket Singh <sanketsingh@Sankets-MacBook-Pro.local>
2022-04-30 14:29:21 +0530

    Setup for basic user scehma with pass encryption done

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -15,6 +16,20 @@ const userSchema = new mongoose.Schema({
         required: true
     }
 }, {timestamps: true});
+
+// pre - save - is a trigger that gets a function and executes it before a user object is saved
+userSchema.pre('save', async function encryptPassword (next) {
+    const user = this;
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
+    next();
+});
+
+userSchema.methods.isValidPassword = async function checkValidity(password) {
+    const user = this;
+    const compare = await bcrypt.compare(password, user.password);
+    return compare;
+}
 
 const User = mongoose.model('User', userSchema);
 
